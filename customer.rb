4b89e06603285cb9fe1b5069add5fc680563b479
Alex Mercea <alex@lxmrc.com>
2022-03-04 15:54:21 +1100

    Add tests

--- customer.rb
+++ customer.rb
@@ -11,9 +11,9 @@ class Customer
   end
 
   def statement
-    total_amount = 0
-    frequent_renter_points = 0
+    total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
+
     @rentals.each do |element|
       this_amount = 0
 
@@ -31,11 +31,17 @@ class Customer
 
       # add frequent renter points
       frequent_renter_points += 1
+
       # add bonus for a two day new release rental
-      frequent_renter_points += 1 if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+        frequent_renter_points += 1
+      end
+
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      total_amount += this_amount
     end
+
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
---
+++ customer_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require_relative 'movie'
+require_relative 'rental'
+require_relative 'customer'
+
+class CustomerTest < Minitest::Test
+  def setup
+    @movie_1 = Movie.new("Dune", Movie::NEW_RELEASE)
+    @movie_2 = Movie.new("Star Wars", Movie::REGULAR)
+    @movie_3 = Movie.new("The Incredibles", Movie::CHILDRENS)
+
+    @rental_1 = Rental.new(@movie_1, 2)
+    @rental_2 = Rental.new(@movie_2, 3)
+    @rental_3 = Rental.new(@movie_3, 4)
+
+    @customer = Customer.new("Alex")
+    @customer.add_rental(@rental_1)
+    @customer.add_rental(@rental_2)
+    @customer.add_rental(@rental_3)
+  end
+
+  def test_statement_generates_a_statement
+    expected = <<~STATEMENT
+      Rental Record for Alex
+      \tDune\t6
+      \tStar Wars\t3.5
+      \tThe Incredibles\t3.0
+      Amount owed is 12.5
+      You earned 4 frequent renter points
+    STATEMENT
+
+    assert_equal @customer.statement, expected.chomp
+  end
+end
